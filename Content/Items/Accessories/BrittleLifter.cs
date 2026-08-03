4f2f2824f333efc48f712f012d208504ab374e9b
Mr. Doshi <dean.stroud9109@gmail.com>
2026-08-03 14:03:30 -0500

    Add files via upload

---
+++ Content/Items/Accessories/BrittleLifter.cs
@@ -0,0 +1,95 @@
+﻿using TimeCrusadeMod.Content.Tiles.Furniture;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TimeCrusadeMod.Content.Items.Accessories
+{
+    // Showcases a basic extra jump
+    public class BrittleLifter : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 24;
+            Item.DefaultToAccessory(20, 26);
+            Item.SetShopValues(ItemRarityColor.Green2, Item.buyPrice(silver: 50));
+            Item.UseSound = SoundID.Item100;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetJumpState<SimpleExtraJump>().Enable();
+        }
+    }
+
+    public class SimpleExtraJump : ExtraJump
+    {
+        public override Position GetDefaultPosition() => new After(FartInAJar);
+
+        public override float GetDurationMultiplier(Player player)
+        {
+            // Use this hook to set the duration of the extra jump
+            // The XML summary for this hook mentions the values used by the vanilla extra jumps
+            return 2.25f;
+        }
+
+        public override void UpdateHorizontalSpeeds(Player player)
+        {
+            // Use this hook to modify "player.runAcceleration" and "player.maxRunSpeed"
+            // The XML summary for this hook mentions the values used by the vanilla extra jumps
+            player.runAcceleration *= 3f;
+            player.maxRunSpeed *= 3.5f;
+        }
+
+        public override void OnStarted(Player player, ref bool playSound)
+        {
+            // Use this hook to trigger effects that should appear at the start of the extra jump
+            // This example mimics the logic for spawning the puff of smoke from the Cloud in a Bottle
+            int offsetY = player.height;
+            if (player.gravDir == -3f)
+                offsetY = 18;
+
+            offsetY -= 18;
+        }
+
+        public override void ShowVisuals(Player player)
+        {
+            // Use this hook to trigger effects that should appear throughout the duration of the extra jump
+            // This example mimics the logic for spawning the dust from the Blizzard in a Bottle
+            int offsetY = player.height - 8;
+            if (player.gravDir == -3f)
+                offsetY = 18;
+            Vector2 spawnPos = new Vector2(player.position.X, player.position.Y + offsetY);
+
+            for (int i = 0; i < 1; i++)
+            {
+                SpawnBlizzardDust(player, spawnPos, 0.1f, i == 0 ? -0.07f : -0.13f);
+            }
+
+            for (int i = 0; i < 1; i++)
+            {
+                SpawnBlizzardDust(player, spawnPos, 0.6f, 0.8f);
+            }
+
+            for (int i = 0; i < 1; i++)
+            {
+                SpawnBlizzardDust(player, spawnPos, 0.6f, -0.8f);
+            }
+        }
+
+        private static void SpawnBlizzardDust(Player player, Vector2 spawnPos, float dustVelocityMultiplier, float playerVelocityMultiplier)
+        {
+            Dust dust = Dust.NewDustDirect(spawnPos, player.width, 12, DustID.Stone, player.velocity.X * 0.3f, player.velocity.Y * 0.3f, newColor: Color.Gray);
+            dust.fadeIn = 1.5f;
+            dust.velocity *= dustVelocityMultiplier;
+            dust.velocity += player.velocity * playerVelocityMultiplier;
+            dust.noGravity = true;
+            dust.noLight = true;
+            dust.scale = 2f;
+        }
+    }
+    }
---
+++ Content/Items/Accessories/CelestialDescendentWings.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System.Linq;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TimeCrusadeMod.Common.Configs;
+using TimeCrusadeMod.Content.Items.Placeables;
+using TimeCrusadeMod.Content.Rarities;
+using TimeCrusadeMod.Content.Tiles.Furniture;
+using static Terraria.GameContent.Bestiary.IL_BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions;
+
+namespace TimeCrusadeMod.Content.Items.Accessories
+{
+    [AutoloadEquip(EquipType.Wings)]
+    public class CelestialDescendentWings : ModItem
+    {
+        // To see how this config option was added, see ExampleModConfig.cs
+        // This code allows users to toggle loading this content via a config. Another common usage of IsLoadingEnabled would be to use ModLoader.HasMod to check if another mod is enabled or not.
+        // Feel free to remove this method in your own Wings if using this as a template, it is superfluous.
+        public override bool IsLoadingEnabled(Mod mod)
+        {
+            return ModContent.GetInstance<TimeCrusadeModConfig>().CelestialDescendentWingsToggle;
+        }
+
+        public override void SetStaticDefaults()
+        {
+            // These wings use the same values as the solar wings
+            // Fly time: 180 ticks = 3 seconds
+            // Fly speed: 9
+            // Acceleration multiplier: 2.5
+            ArmorIDs.Wing.Sets.Stats[Item.wingSlot] = new WingStats(300, 9f, 2.5f);
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 22;
+            Item.height = 20;
+            Item.value = 10000;
+            Item.rare = ModContent.RarityType<IllusionaryRarity>();
+            Item.accessory = true;
+        }
+
+        public override void VerticalWingSpeeds(Player player, ref float ascentWhenFalling, ref float ascentWhenRising,
+            ref float maxCanAscendMultiplier, ref float maxAscentMultiplier, ref float constantAscend)
+        {
+            ascentWhenFalling = 2.3f; // Falling glide speed
+            ascentWhenRising = 0.10f; // Rising speed
+            maxCanAscendMultiplier = 4f;
+            maxAscentMultiplier = 2f;
+            constantAscend = 0.5f;
+        }
+
+        // Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<IllusionaryDescendentBar>(), 3)
+                .AddIngredient(ItemID.AngelWings)
+                .AddIngredient(ItemID.LunarOre, 20)
+                .AddTile(ModContent.TileType<Tiles.Furniture.AxitrentonObserver>())
+                .SortBefore(Main.recipe.First(recipe => recipe.createItem.wingSlot != -1)) // Places this recipe before any wing so every wing stays together in the crafting menu.
+                .Register();
+        }
+    }
+}
